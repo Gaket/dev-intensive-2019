8b8fe0c3058208bbacc1543c63de64107b68864a
Artur Badretdinov <artursletter@gmail.com>
2019-07-10 21:42:00 +0300

    Update Base message and its children

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -7,12 +7,13 @@ import java.util.*
  * Created by Artur (gaket) on 2019-07-04.
  */
 class TextMessage(
-        id: String,
-        from: User?,
-        chat: Chat,
-        isIncoming: Boolean = false,
-        date: Date = Date(),
-        val text: String?
+    id: String,
+    from: User?,
+    chat: Chat,
+    isIncoming: Boolean = false,
+    date: Date = Date(),
+    var text: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage():String = "id:$id ${from?.firstName} ${if(isIncoming) "received" else "sent"} a message \"$text\" ${date.humanizeDiff()}"
+    override fun formatMessage(): String =
+        "id:$id ${from?.firstName} ${if (isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}."
 }
