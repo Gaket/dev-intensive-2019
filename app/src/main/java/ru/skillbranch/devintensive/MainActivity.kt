fcd2bdfbc0a023755184fb6e7c7aa7cb28efa9ca
Artur Badretdinov <artursletter@gmail.com>
2019-07-14 23:43:08 +0300

    Add question handling

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,13 +1,20 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var benderObj: Bender
 
     private lateinit var sendBtn: ImageView
     private lateinit var messageEt: EditText
@@ -22,5 +29,19 @@ class MainActivity : AppCompatActivity() {
         textTxt = tv_text
         messageEt = et_message
         sendBtn = iv_send
+
+        benderObj = Bender()
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            textTxt.setText(phrase)
+            messageEt.setText("")
+            val (r,g,b) = color
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+        }
     }
 }
