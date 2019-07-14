7022b286c801ec0b982067df9067ddf8ff1e5121
Artur Badretdinov <artursletter@gmail.com>
2019-07-14 14:01:30 +0300

    Add Main Activity layout

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,11 +2,25 @@ package ru.skillbranch.devintensive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sendBtn: ImageView
+    private lateinit var messageEt: EditText
+    private lateinit var benderImage: ImageView
+    private lateinit var textTxt: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
     }
 }
