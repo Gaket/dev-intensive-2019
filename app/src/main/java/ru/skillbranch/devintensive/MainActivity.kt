26edddab38ef741e72ac4f5911018a5357bf090a
Artur Badretdinov <artursletter@gmail.com>
2019-07-25 23:57:21 +0300

    Add rules to dependencies

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
