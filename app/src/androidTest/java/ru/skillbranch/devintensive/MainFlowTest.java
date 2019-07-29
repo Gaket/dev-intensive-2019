c096119fca7fefad82051d3d2723009ae4d9e895
Artur Badretdinov <artursletter@gmail.com>
2019-07-29 22:48:08 +0300

    Basic tests structure

--- app/src/androidTest/java/ru/skillbranch/devintensive/MainFlowTest.java
+++ app/src/androidTest/java/ru/skillbranch/devintensive/MainFlowTest.java
@@ -15,22 +15,49 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainFlowTest {
-
     @Rule
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void firstTest() {
-        Espresso.onView(ViewMatchers.withId(R.id.tv_text))
-                .check(ViewAssertions.matches(ViewMatchers.withText("Как меня зовут?")));
+    public void onAppStart_nameQuestionIsDisplayed() {
+
+        // Find view                Arrange
+        // Perform an action        Act
+        // Assert correctness       Assert
+
+        Espresso.onView(ViewMatchers.withText("Как меня зовут?"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
     }
 
     @Test
-    public void secondTest() {
+    public void onAppStart_benderIsDisplayed() {
+
+        // Find view                Arrange
+        // Perform an action        Act
+        // Assert correctness       Assert
+
+        Espresso.onView(ViewMatchers.withId(R.id.iv_bender))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+    }
+
+    @Test
+    public void afterCorrectAnswer_professionQuestionIsAsked() {
+
         Espresso.onView(ViewMatchers.withId(R.id.et_message))
                 .perform(ViewActions.click());
 
         Espresso.onView(ViewMatchers.withId(R.id.et_message))
                 .perform(ViewActions.typeText("Bender"));
+
+        Espresso.onView(ViewMatchers.withId(R.id.iv_send))
+                .perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.iv_send))
+                .perform(ViewActions.closeSoftKeyboard());
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_text))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Отлично - ты справился\nНазови мою профессию?")));
     }
 }
